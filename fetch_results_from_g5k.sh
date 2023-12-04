0f060e820f0a34b733129fe6845004f16edec808
aomond <antoine.omond@imt-atlantique.fr>
2023-12-04 09:48:19 +0100

    Moved fetch results script to g5k-scripts repo

--- fetch_results_from_g5k.sh
+++
@@ -1,7 +0,0 @@
-archive_name='topologies_results.tar.xz'
-ssh nancy.grid5000.fr "tar --lzma -cf $archive_name -C results-reconfiguration-esds topologies/paper"
-scp "nancy.grid5000.fr:/home/anomond/$archive_name" "$archive_name"
-ssh nancy.grid5000.fr "rm $archive_name"
-
-tar --lzma -xvf "./$archive_name"
-rm "$archive_name"
