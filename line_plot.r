cf44f9cbbe86efdbacfa110cf320eeb82c5d528d
aomond <antoine.omond@imt-atlantique.fr>
2023-12-24 14:45:38 +0800

    plot csv r

---
+++ line_plot.r
@@ -0,0 +1,9 @@
+library("ggplot2")
+d <- read.csv("e_time.csv")
+
+ggplot(d, aes(x=size, y=energy_mean, colour=net_tplgy)) +
+    geom_errorbar(aes(ymin=energy_mean - energy_std, ymax=energy_mean + energy_std), width=.1) +
+    geom_line() +
+    geom_point() + facet_wrap(~ srv_tplgy + rn_type) + xlab("Number of Observation Nodes") + ylab("Accumulated energy mean (J)") + scale_x_continuous(breaks=unique(d$size))
+
+ggsave("e_time.pdf", width=20, height=20)
