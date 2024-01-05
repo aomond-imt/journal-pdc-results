55e3cea62dd944dec0a10d46e8819fe173a68eb6
aomond <antoine.omond@imt-atlantique.fr>
2024-01-05 15:43:33 +0100

    Refacto/simplification

--- result_analysis.py
+++ result_analysis.py
@@ -89,63 +89,63 @@ rn = ["no_rn", "rn_agg", "rn_not_agg"]
 colors = ["blue", "orange"]
 srv = ["fav", "nonfav"]
 
-m = {
+srv_conv = {
     "fav": "best",
     "nonfav": "worst",
-    "no_rn": "No RN",
-    "rn_agg": "RN on agg",
-    "rn_not_agg": "RN not on agg"
 }
+
+rn_conv = {
+    "no_rn": {"fav": "No RN", "nonfav": "No RN"},
+    "rn_agg": {"fav": "best", "nonfav": "worst"},
+    "rn_not_agg": {"fav": "worst", "nonfav": "best"},
+}
+
+unit = {
+    "total": 1000,
+    "dynamic": 1,
+    "time": 3600
+}
+
 
 # indexes = [("fav","no_rn"), ("nonfav","no_rn"), ("fav","rn_agg"), ("nonfav","rn_agg"), ("fav","rn_not_agg"), ("nonfav","rn_not_agg")]
 indexes = [("fav","clique"), ("nonfav","clique"), ("fav","star"), ("nonfav","star"), ("fav","grid"), ("nonfav","grid"), ("fav","ring"), ("nonfav","ring"), ("fav","chain"), ("nonfav","chain")]
 indexes_rn = [("No RN", "fav"), ("No RN", "nonfav"), ("best", "fav"), ("best", "nonfav"), ("worst", "fav"), ("worst", "nonfav")]
+
+
+def p(gb, energy_type):
+    list_gb = copy.deepcopy(columns)
+    list_gb.remove("res")
+    list_gb.remove(gb)
+    list_gb = ["rn_type"]
+    df = get_df()
+
+    df_gb = [*df.groupby(list_gb)]
+    for key, pandas_vals in df_gb:
+        vals_to_print = filter(lambda el: el[0] not in ["starchain", "tree"], pandas_vals.values)
+        results = sorted(vals_to_print, key=lambda el: (el[3], rn.index(el[2]), net.index(el[0]), srv.index(el[1])))
+        for res in results:
+            net_tply, srv_tplgy, rn_type, size, values = res
+            div = unit[energy_type]
+            rn_pos = rn_conv[rn_type][srv_tplgy]
+            index = indexes.index((srv_tplgy, net_tply))
+            index_rn = indexes_rn.index((rn_pos, srv_tplgy))
+            csvwriter.writerow([
+                net_tply,
+                size,
+                f"{values[energy_type].mean() / div:.2f}",
+                f"{values[energy_type].std() / div:.2f}",
+                srv_conv[srv_tplgy],
+                rn_pos,
+                index_rn
+            ])
+
 
 for energy_type in ["total", "dynamic", "time"]:
     csvfile_name = f"e_{energy_type}.csv"
     csvfile = open(csvfile_name, "w")
     csvwriter = csv.writer(csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
     csvwriter.writerow(["net_tplgy", "size", "energy_mean", "energy_std", "srv_tplgy", "rn_type", "srv_tplgy_index"])
-    for rn_type in ["no_rn", "rn_agg", "rn_not_agg"]:
-    # rn_type = "rn_not_agg"
-        def p(gb, rn_type, energy_type):
-            list_gb = copy.deepcopy(columns)
-            list_gb.remove("res")
-            list_gb.remove(gb)
-            list_gb = ["size"]
-            df = get_df()
-
-            for key, pandas_vals in df.groupby(list_gb):
-                vals_to_print = filter(lambda el: el[2] in [rn_type] and el[0] not in ["starchain", "tree"], pandas_vals.values)
-                results = sorted(vals_to_print, key=lambda el: (net.index(el[0]), srv.index(el[1]), rn.index(el[2])))
-                for res_num in range(len(results)):
-                    values = results[res_num][-1]
-                    if energy_type == "total":
-                        div = 1000
-                    elif energy_type == "time":
-                        div = 3600
-                    else:
-                        div = 1
-                    rn_pos = ""
-                    if rn_type == "no_rn":
-                        rn_pos = "No RN"
-                    if rn_type == "rn_agg":
-                        if results[res_num][1] == "fav":
-                            rn_pos = "best"
-                        if results[res_num][1] == "nonfav":
-                            rn_pos = "worst"
-                    if rn_type == "rn_not_agg":
-                        if results[res_num][1] == "fav":
-                            rn_pos = "worst"
-                        if results[res_num][1] == "nonfav":
-                            rn_pos = "best"
-                    if rn_pos == "":
-                        raise Exception("rn_pos none")
-                    index = indexes.index((results[res_num][1], results[res_num][0]))
-                    index_rn = indexes_rn.index((rn_pos, results[res_num][1]))
-                    csvwriter.writerow([results[res_num][0], results[res_num][3], f"{values[energy_type].mean()/div:.2f}", f"{values[energy_type].std()/div:.2f}", m[results[res_num][1]], rn_pos, index_rn])
-
-        p("size", rn_type, energy_type)
-
+    # for rn_type in ["no_rn", "rn_agg", "rn_not_agg"]:
+    p("size", energy_type)
     csvfile.close()
     print(f"csv: {csvfile_name}")
