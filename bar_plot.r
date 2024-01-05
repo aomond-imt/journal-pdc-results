ae5cbd3645f20b23875b40d41cb9b2dbf96c4287
aomond <antoine.omond@imt-atlantique.fr>
2024-01-05 12:58:35 +0100

    Final generation plots

--- bar_plot.r
+++ bar_plot.r
@@ -7,38 +7,29 @@ library("dplyr")
 # for (t in c("clique", "star", "grid", "ring", "chain")){
 for (t in c("No RN", "RN on agg", "RN not on agg")){
     for (e_type in c("dynamic", "total")) {
-#         t <- "RN on agg"
-#         e_type <- "total"
-        d <- read.csv(glue::glue("e_{e_type}.csv"))
-#         print(d)
-#         d<-d %>% filter(net_tplgy == t)
-        d<-d %>% filter(rn_type == t)
-        d<-d %>% filter(size == 9)
-#         d$srv_rn_tplgy <- paste(d$srv_tplgy,d$rn_type)
-#         k<-arrange(d, rn_type, srv_tplgy)
-        unit <- if(e_type == "dynamic") "J" else "kJ"
-        if (e_type == "dynamic") {
-            color=function(){scale_fill_manual(values = c("#00AFBB", "#E7B800", "#FC4E07"))}
+        for (s in c(9, 25)) {
+            d <- read.csv(glue::glue("e_{e_type}.csv"))
+            d<-d %>% filter(rn_type == t)
+            d<-d %>% filter(size == s)
+            unit <- if(e_type == "dynamic") "J" else "kJ"
+            if (e_type == "dynamic") {
+                color=function(){scale_fill_manual(values = c("#00AFBB", "#E7B800", "#FC4E07"))}
+            }
+            else {
+                color=function(){scale_fill_manual(values = c("#b1c470", "#8370c4", "#FC4E07"))}
+            }
+            ggplot(d, aes(x=reorder(net_tplgy, srv_tplgy_index), y=energy_mean, fill=srv_tplgy, label=energy_mean)) +
+                geom_bar(color="black", stat = "identity", position = "dodge") +
+                geom_errorbar(position=position_dodge(0.9), aes(ymin=energy_mean - energy_std, ymax=energy_mean + energy_std), width=.2, size=1) +
+                geom_text(aes(y=min(energy_mean)/2), fill="white", fontface="bold", position=position_dodge(width=0.9)) +
+                ylab(glue::glue("Accumulated energy consumption ({unit})")) +
+                theme(axis.text.y = element_text()) +
+                xlab(element_blank()) +
+                labs(fill="Aggregation ON position:") +
+                theme(legend.position="top", legend.text=element_text(size=15), legend.title=element_text(size=15), axis.text=element_text(size=15), axis.title.y=element_text(size=15, vjust=2.5)) +
+                color()
+            ggsave(glue::glue("bar_plot/{e_type}/{s}/{t}.pdf"), width=9, height=6)
         }
-        else {
-            color=function(){scale_fill_manual(values = c("#b1c470", "#8370c4", "#FC4E07"))}
-        }
-        # color=function(){scale_fill_manual(values = c("#b1c470", "#8370c4", "#FC4E07"))}
-#         ggplot(d, aes(x=reorder(net_tplgy, srv_tplgy_index), y=energy_mean, fill=srv_tplgy, label=energy_mean)) +
-        ggplot(d, aes(x=reorder(net_tplgy, srv_tplgy_index), y=energy_mean, fill=srv_tplgy, label=energy_mean)) +
-            geom_bar(color="black", stat = "identity", position = "dodge") +
-            geom_errorbar(position=position_dodge(0.9), aes(ymin=energy_mean - energy_std, ymax=energy_mean + energy_std), width=.2, size=1) +
-#             geom_text(aes(y=min(energy_mean)/2), fill="white", fontface="bold", position=position_dodge(width=0.9)) +
-#             geom_text(aes(y=2), fontface="bold", position=position_dodge(width=0.9)) +
-            geom_text(aes(label = energy_mean, y = min(energy_mean)/2),vjust=-0.4,position=position_dodge(0.9),fontface="bold") +
-        #     geom_label(data=d,aes(label = d$rn_type, y=35),label.padding=unit(0.35,"lines"),label.r=unit(0.09,"lines"),fill="#f0f0f0",label.size=0.5,fontface="bold",colour="black",position=position_dodge(0.9))+
-            ylab(glue::glue("Accumulated energy consumption ({unit})")) +
-            theme(axis.text.y = element_text()) +
-            xlab(element_blank()) +
-            labs(fill="Aggregation ON position:") +
-            theme(legend.position="top", legend.text=element_text(size=15), legend.title=element_text(size=15), axis.text=element_text(size=15), axis.title.y=element_text(size=15, vjust=2.5)) +
-            color()
-        ggsave(glue::glue("plots_paper_per_tplgy/{e_type}/{t}.pdf"), width=8, height=6)
     }
 }
 
--- line_plot_rn_type.r
+++ line_plot_rn_type.r
@@ -1,21 +1,39 @@
 library("ggplot2")
 library("dplyr")
 
+# color=function(){scale_color_manual(values = c("#00AFBB", "#E7B800", "#FC4E07"))}
 
-e = "dynamic"
+e = "total"
+dodge = 5.5
 for (t in c("clique", "star", "grid", "ring", "chain")) {
     d <- read.csv(glue::glue("e_{e}.csv"))
     d<-d %>% filter(net_tplgy == t)
-    d<-d %>% filter(srv_tplgy == "worst")
-    ggplot(d, aes(x=size, y=energy_mean, colour=rn_type)) +
-        geom_errorbar(aes(ymin=energy_mean - energy_std, ymax=energy_mean + energy_std), width=.1, position=position_dodge(0.5)) +
-        geom_line() +
-        geom_point(position=position_dodge(0.5)) +
+#     d<-d %>% filter(srv_tplgy == "worst")
+    unit <- if(e == "dynamic") "J" else "kJ"
+    if (e == "dynamic") {
+        color=function(){scale_color_manual(values = c("#00AFBB", "#E7B800", "#FC4E07"))}
+    }
+    else {
+        color=function(){scale_color_manual(values = c("#b1c470", "#8370c4", "#FC4E07"))}
+    }
+#     d<-d %>% filter(srv_tplgy == "worst")
+    ggplot(d, aes(x=size, y=energy_mean, shape=reorder(rn_type, srv_tplgy_index), colour=srv_tplgy)) +
+        geom_errorbar(aes(ymin=energy_mean - energy_std, ymax=energy_mean + energy_std), width=.1, position=position_dodge(dodge), show.legend=FALSE) +
+        geom_point(position=position_dodge(dodge)) +
         xlab("Number of ONs") +
-        ylab("Accumulated energy consumption (J)") +
+        ylab(glue::glue("Accumulated energy consumption ({unit})")) +
         scale_x_continuous(breaks=unique(d$size)) +
-        labs(colour="Physical topology:") +
-        theme(legend.position="top")
+        labs(colour="Physical topology:", shape="Type of RN:") +
+#         theme(legend.position="top") +
+        theme(legend.spacing = unit(-5, "pt"), legend.position="none", legend.box = "vertical",
+              legend.key.size=unit(5, "points"), legend.text=element_text(size=5), legend.title=element_text(size=6), axis.text=element_text(size=7),
+              axis.title.y=element_text(size=7, vjust=2.5), axis.title.x=element_text(size=7),
+              panel.grid.minor.x=element_blank()) +
+        guides(colour = guide_legend(override.aes = list(shape = 15))) +
 
-    ggsave(glue::glue("line_plot_rn_type/{e}/{t}.pdf"), width=6, height=4)
+#         scale_color_manual(values=c("#CC6666", "#9999CC"))
+#         scale_y_continuous(n.breaks=12) +
+        color()
+
+    ggsave(glue::glue("line_plot_rn_type/{e}/{t}.pdf"), width=2, height=3)
 }
